f2baba77803a31bfc30b7e210da29ba1e5ee2004
Mauro Alvarez <contact@mauroalvarez.com>
2023-07-28 11:39:03 -0300

    Update popup to display participating websites data

--- popup.tsx
+++ popup.tsx
@@ -1,28 +1,88 @@
-import { useState } from "react"
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "@/components/ui/table";
+import { StorageKey } from "@/lib/storageKey";
+import type {
+  participantDataType,
+  participantsDataType
+} from "@/types/participantsData";
+import { ChevronLeft } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { useStorage } from "@plasmohq/storage/hook";
+
+import "@/style.css";
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [selectedParticipant, setSelectedParticipant] =
+    useState<participantDataType | null>(null);
+  const [participantsData] = useStorage<participantsDataType>(
+    StorageKey.ParticipantsData
+  );
+
+  useEffect(() => {
+    console.log("participantsData", participantsData);
+  }, [participantsData]);
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        padding: 16
+        padding: 16,
+        width: 400
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      {selectedParticipant == null ? (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="items-center">
+                Participating Websites
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {participantsData?.websites.map((participantData) => (
+              <TableRow
+                style={{ cursor: "pointer" }}
+                onClick={() => setSelectedParticipant(participantData)}>
+                <TableCell className="font-medium">
+                  {participantData.name}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="items-center">Messages</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {selectedParticipant.messages.map((message) => (
+                <TableRow>
+                  <TableCell className="font-medium">{message}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <Button onClick={() => setSelectedParticipant(null)}>
+            <ChevronLeft className="mr-2 h-4 w-4" />
+            Go Back
+          </Button>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
